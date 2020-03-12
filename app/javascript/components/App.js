fb48aae81fbdce2a0354968db548ad294f9a40dd
Shannon Menard <menard01@email.franklin.edu>
2020-03-11 22:28:27 -0400

    Added Accessories section with title, paragraph, picture and button

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -83,6 +83,8 @@ export default function App(props) {
         </Router>
 
 
+
+
     )
 
 }
--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -7,11 +7,14 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import HeroImage from '../assets/images/heroimage.jpg';
+import Accessories from '../assets/images/caraccessories.jpg';
 import {Container} from "@material-ui/core";
 import Paper from "@material-ui/core/Paper";
 import Box from "@material-ui/core/Box";
 import Navbar from "./Navbar";
-
+import Grid from '@material-ui/core/Grid';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Divider from "@material-ui/core/Divider";
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -30,7 +33,8 @@ const useStyles = makeStyles(theme => ({
         backgroundPosition: 'center',
         backgroundSize: 'cover',
         height: '32vh',
-        textAlign: 'center'
+        textAlign: 'center',
+        top: '0px',
     },
     pageHeroText: {
         color: '#FFFFFF',
@@ -38,7 +42,17 @@ const useStyles = makeStyles(theme => ({
         textAlign: 'center',
         paddingTop: theme.spacing(2)
     },
+    firstSection: {
+        marginTop: '5%',
+    },
+    accessories: {
+        flexGrow: 1,
+        paddingLeft: '6%',
+        paddingBottom: '10px',
+    },
+    accessoriesText: {
 
+    },
 
 }));
 
@@ -46,20 +60,55 @@ export default function ButtonAppBar() {
     const classes = useStyles();
 
     return (
-        <Container className={classes.root}>
-            <Box m={1}>
-                <Paper className={classes.pageHero} style={{justifyContent: 'center'}}>
-                    <Typography variant="h3" className={classes.pageHeroText} mt={4}>
-                        Welcome
-                    </Typography>
-                    <Box m={4}>
+
+        // main hero section
+
+            <React.Fragment>
+                <div>
+            <Container maxWidth="xl">
+                <CssBaseline />
+                <Box m={1}>
+                    <Paper className={classes.pageHero} style={{justifyContent: 'center'}}>
+                        <Typography variant="h3" className={classes.pageHeroText} mt={4}>
+                            Welcome
+                        </Typography>
+                        <Box m={4}>
+                            <Button variant="contained" color="secondary">
+                                Shop Now
+                            </Button>
+                        </Box>
+                    </Paper>
+                </Box>
+            </Container>
+
+                <Grid container spacing={3} className={classes.firstSection}>
+                    <Grid item xs={6}>
+                        <Typography variant="h4" className={classes.accessories}>
+                            Accessories
+                        </Typography>
+                        <Typography variant="p" className={classes.accessoriesText}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Urna condimentum mattis pellentesque id nibh tortor id aliquet lectus.
+                            Egestas maecenas pharetra convallis posuere morbi leo urna molestie. Nullam
+                            vehicula ipsum a arcu cursus. Praesent elementum facilisis leo vel.
+
+                        </Typography>
+                        <div style={{marginTop: '25%', marginLeft: '25%'}}>
                         <Button variant="contained" color="secondary">
-                            Shop Now
+                            See Our Accessories
                         </Button>
-                    </Box>
-                </Paper>
-            </Box>
-        </Container>
+                        </div>
+                    </Grid>
+
+                    <Grid item xs={6}>
+                       <img src={Accessories} height="400vh" width="500vh"/>
+                    </Grid>
+                </Grid>
+                </div>
+            </React.Fragment>
+
+
 
 
     );
