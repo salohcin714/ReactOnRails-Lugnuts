dc268fe2e4b24097479861ec40bddca81454425c
Nicholas Norris <norris48@email.franklin.edu>
2020-03-12 21:19:21 -0400

    Fixed Homepage and Nav

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import HeroImage from '../assets/images/heroimage.jpg';
 import Accessories from '../assets/images/caraccessories.jpg';
 import Product1 from '../assets/images/product1.jpg';
 import {Container} from "@material-ui/core";
 import Paper from "@material-ui/core/Paper";
 import Box from "@material-ui/core/Box";
-import Navbar from "./Navbar";
+import {Link} from 'react-router-dom'
+import CardMedia from "@material-ui/core/CardMedia";
 import Grid from '@material-ui/core/Grid';
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Chip from '@material-ui/core/Chip';
 import TextField from '@material-ui/core/TextField';
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import IconButton from "@material-ui/core/IconButton";
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import useTheme from "@material-ui/core/styles/useTheme";
+import CardActions from "@material-ui/core/CardActions";
+
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -35,8 +41,7 @@ const useStyles = makeStyles(theme => ({
         backgroundPosition: 'center',
         backgroundSize: 'cover',
         height: '32vh',
-        textAlign: 'center',
-        top: '0px',
+        textAlign: 'center'
     },
     pageHeroText: {
         color: '#FFFFFF',
@@ -44,107 +49,127 @@ const useStyles = makeStyles(theme => ({
         textAlign: 'center',
         paddingTop: theme.spacing(2)
     },
-    firstSection: {
-        marginTop: '5%',
+    linkFix: {
+        textDecoration: 'none',
+        color: "#FFFFFF"
+
+    },
+    cardLayout: {
+        display: "flex",
+        flexDirection: "column"
     },
     accessories: {
-        flexGrow: 1,
-        paddingLeft: '6%',
-        paddingBottom: '10px',
+        backgroundImage: "url(" + Accessories + ")",
+        height: "32vh"
     },
-    quantityBox: {
-        width: 60,
+    details: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    content: {
+        flex: '1 0 auto',
+    },
+    controls: {
+        display: 'flex',
+        alignItems: 'center',
+        paddingLeft: theme.spacing(1),
+        paddingBottom: theme.spacing(1),
+    },
+    playIcon: {
+        height: 38,
+        width: 38,
     },
 
 }));
 
 export default function ButtonAppBar() {
     const classes = useStyles();
-
+    const theme = useTheme();
     return (
-
-        // main hero section
-
-            <React.Fragment>
-                <div>
-            <Container maxWidth="xl">
-                <CssBaseline />
-                <Box m={1}>
-                    <Paper className={classes.pageHero} style={{justifyContent: 'center'}}>
-                        <Typography variant="h3" className={classes.pageHeroText} mt={4}>
-                            Welcome
-                        </Typography>
-                        <Box m={4}>
-                            <Button variant="contained" color="secondary">
+        <Container className={classes.root}>
+            <Box m={1} mt={3}>
+                <Paper className={classes.pageHero} style={{justifyContent: 'center'}}>
+                    <Typography variant="h3" className={classes.pageHeroText} mt={4}>
+                        Welcome
+                    </Typography>
+                    <Box m={4}>
+                        <Link to={"/shop"} className={classes.linkFix}>
+                            <Button variant="contained" color="secondary" size={"large"}>
                                 Shop Now
                             </Button>
-                        </Box>
-                    </Paper>
-                </Box>
-            </Container>
+                        </Link>
+                    </Box>
+                </Paper>
+            </Box>
+            <Box m={1} mt={3}>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} sm={6}>
+                        <Card className={classes.root}>
+                            <CardMedia
+                                component="img"
+                                alt="Contemplative Reptile"
+                                height="320"
+                                image={Accessories}
+                                title="Contemplative Reptile"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h4" component="h2">
+                                    Lizard
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                    Urna condimentum mattis pellentesque id nibh tortor id aliquet lectus.
+                                    Egestas maecenas pharetra convallis posuere morbi leo urna molestie. Nullam
+                                    vehicula ipsum a arcu cursus. Praesent elementum facilisis leo vel.
+                                </Typography>
+                            </CardContent>
 
-                <Grid container spacing={3} className={classes.firstSection}>
-                    <Grid item xs={6}>
-                        <Typography variant="h4" className={classes.accessories}>
-                            Accessories
-                        </Typography>
-                        <Typography variant="p" className={classes.accessoriesText}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            Urna condimentum mattis pellentesque id nibh tortor id aliquet lectus.
-                            Egestas maecenas pharetra convallis posuere morbi leo urna molestie. Nullam
-                            vehicula ipsum a arcu cursus. Praesent elementum facilisis leo vel.
+                            <CardActions>
 
-                        </Typography>
-                        <div style={{marginTop: '25%', marginLeft: '25%'}}>
-                        <Button variant="contained" color="secondary">
-                            See Our Accessories
-                        </Button>
-                        </div>
+                                <Button size="small" color="primary">
+                                    View Accessories
+                                </Button>
+
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <Card className={classes.root}>
+                            <CardMedia
+                                component="img"
+                                alt="Contemplative Reptile"
+                                height="320"
+                                image={Product1}
+                                title="Contemplative Reptile"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h4" component="h2">
+                                    Product Name
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                    Urna condimentum mattis pellentesque id nibh tortor id aliquet lectus.
+                                    Egestas maecenas pharetra convallis posuere morbi leo urna molestie. Nullam
+                                    vehicula ipsum a arcu cursus. Praesent elementum facilisis leo vel.
+                                </Typography>
+                            </CardContent>
+
+                            <CardActions>
+                                <Chip label={"$X.XX"}/>
+                                <Button size="small" color="primary">
+                                    Add to Cart
+                                </Button>
+
+                            </CardActions>
+                        </Card>
                     </Grid>
 
-                    <Grid item xs={6}>
-                       <img src={Accessories} height="400vh" width="500vh"/>
-                    </Grid>
+
                 </Grid>
-
-
-
-                 <Grid container spacing={3} className={classes.firstSection}>
-                     <Grid item xs={6}>
-                         <img src={Product1} height="400vh" width="500vh" />
-                     </Grid>
-
-                     <Grid item xs={6}>
-                         <Typography variant="h4">Product Name</Typography>
-                         <Chip
-                             label="$$$$"
-                             clickable
-                             color="primary"
-                             variant="outlined"
-                         />
-                         <Typography variant="caption" display="block">Quantity</Typography>
-                         <form className={classes.root} noValidate autoComplete="off">
-                             <TextField id="outlined-basic" label="1" variant="outlined" className={classes.quantityBox}/>
-                         </form>
-
-                         <div style={{marginTop: '30px'}}>
-                             <Button variant="contained" color="secondary">
-                                 Add To Cart
-                             </Button>
-                         </div>
-
-                         <Typography variant="p" style={{paddingTop: '20px'}} display="block">
-                             Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                             Urna condimentum mattis pellentesque id nibh tortor id aliquet lectus.
-                         </Typography>
-                     </Grid>
-                 </Grid>
-                </div>
-            </React.Fragment>
-
-
+            </Box>
+        </Container>
 
 
     );
