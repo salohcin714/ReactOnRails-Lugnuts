ac3880ff8a7c644efb628cb3310b9e0d97c853ff
Shannon Menard <menard01@email.franklin.edu>
2020-03-13 18:40:14 -0400

    Added about page image and text

--- app/javascript/components/About.js
+++ app/javascript/components/About.js
@@ -18,7 +18,35 @@ export default function About() {
                     <Grid item xs={12}>
                         <img src={About1} height="400vh" width="800vh"/>
                     </Grid>
+
+                    <Grid item xs={12}>
+                        <Typography variant="body1">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Urna condimentum mattis pellentesque id nibh tortor id aliquet lectus.
+                            Egestas maecenas pharetra convallis posuere morbi leo urna molestie. Nullam
+                            vehicula ipsum a arcu cursus. Praesent elementum facilisis leo vel.
+                        </Typography>
+
+                        <Typography variant="body1">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Urna condimentum mattis pellentesque id nibh tortor id aliquet lectus.
+                            Egestas maecenas pharetra convallis posuere morbi leo urna molestie. Nullam
+                            vehicula ipsum a arcu cursus. Praesent elementum facilisis leo vel.
+                        </Typography>
+
+                        <Typography variant="body1">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Urna condimentum mattis pellentesque id nibh tortor id aliquet lectus.
+                            Egestas maecenas pharetra convallis posuere morbi leo urna molestie. Nullam
+                            vehicula ipsum a arcu cursus. Praesent elementum facilisis leo vel.
+                        </Typography>
+                    </Grid>
                 </Grid>
+
+
             </Box>
 
         </Container>
