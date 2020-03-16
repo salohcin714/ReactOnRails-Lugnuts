4476be5c4ce9d3e9796e096de1d37ae1836ca269
Nicholas Norris <41271531+salohcin714@users.noreply.github.com>
2020-03-16 19:17:20 -0400

    Added tabs to Shop page

--- app/javascript/components/Shop.js
+++ app/javascript/components/Shop.js
@@ -4,14 +4,18 @@ import {Container} from "@material-ui/core";
 import Shop1 from '../assets/images/shop1.jpg';
 import Box from "@material-ui/core/Box";
 import Grid from '@material-ui/core/Grid';
-import {makeStyles} from "@material-ui/core/styles";
+import {makeStyles, useTheme} from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import {Link} from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import InputAdornment from '@material-ui/core/InputAdornment';
 import TextField from '@material-ui/core/TextField';
 import SearchIcon from '@material-ui/icons/Search';
-
+import SwipeableViews from 'react-swipeable-views'
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import * as PropTypes from "prop-types";
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -48,12 +52,49 @@ const useStyles = makeStyles(theme => ({
 
 }));
 
+function TabPanel(props) {
+    const {children, value, index, ...other} = props;
+
+    return (
+        <Typography
+            component="div"
+            role="tabpanel"
+            hidden={value !== index}
+            id={`full-width-tabpanel-${index}`}
+            aria-labelledby={`full-width-tab-${index}`}
+            {...other}
+        >
+            {value === index && <Box p={3}>{children}</Box>}
+        </Typography>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `full-width-tab-${index}`,
+        'aria-controls': `full-width-tabpanel-${index}`,
+    };
+}
+
 
 export default function Shop() {
     const classes = useStyles();
+    const theme = useTheme();
+    const [value, setValue] = React.useState(0);
 
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
-
+    const handleChangeIndex = index => {
+        setValue(index);
+    };
     return (
         <Container className={classes.root}>
             <Box m={1} mt={3}>
@@ -86,7 +127,7 @@ export default function Shop() {
                             InputProps={{
                                 startAdornment: (
                                     <InputAdornment position="start">
-                                        <SearchIcon />
+                                        <SearchIcon/>
                                     </InputAdornment>
                                 )
                             }}
@@ -100,11 +141,35 @@ export default function Shop() {
                     <Grid item xs={12}>
 
 
-
                     </Grid>
                 </Grid>
             </Box>
+            <AppBar position="static" color="default">
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    variant="fullWidth"
+                    aria-label="full width tabs example"
+                >
+                    <Tab label="Browse by Product" {...a11yProps(0)} />
+                    <Tab label="Browse by Category" {...a11yProps(1)} />
+                </Tabs>
+            </AppBar>
+            <SwipeableViews
+                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                index={value}
+                onChangeIndex={handleChangeIndex}
+            >
+                <TabPanel value={value} index={0} dir={theme.direction}>
+                    Item One
+                </TabPanel>
+                <TabPanel value={value} index={1} dir={theme.direction}>
+                    Item Two
+                </TabPanel>
 
+            </SwipeableViews>
 
         </Container>
     )
