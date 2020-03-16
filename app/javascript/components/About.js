24e4cd978b4177657f4a15df870459a06a22288d
Nicholas Norris <norris48@email.franklin.edu>
2020-03-16 12:25:39 -0400

    Updated About.js

--- app/javascript/components/About.js
+++ app/javascript/components/About.js
@@ -4,48 +4,52 @@ import {Container} from "@material-ui/core";
 import Box from "@material-ui/core/Box";
 import Grid from '@material-ui/core/Grid';
 import About1 from '../assets/images/about1.jpg';
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
 
 export default function About() {
     return (
         <Container>
-            <Typography variant={"h3"} style={{textAlign: 'center'}}>
-                About Us
-            </Typography>
+
 
             <Box mt={3}>
                 <Grid container spacing={3}>
                     <Grid item xs={12}>
-                        <img src={About1} height="400vh" width="800vh"/>
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        <Typography variant="body1">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            Urna condimentum mattis pellentesque id nibh tortor id aliquet lectus.
-                            Egestas maecenas pharetra convallis posuere morbi leo urna molestie. Nullam
-                            vehicula ipsum a arcu cursus. Praesent elementum facilisis leo vel.
-                        </Typography>
-
-                        <Typography variant="body1">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            Urna condimentum mattis pellentesque id nibh tortor id aliquet lectus.
-                            Egestas maecenas pharetra convallis posuere morbi leo urna molestie. Nullam
-                            vehicula ipsum a arcu cursus. Praesent elementum facilisis leo vel.
-                        </Typography>
-
-                        <Typography variant="body1">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            Urna condimentum mattis pellentesque id nibh tortor id aliquet lectus.
-                            Egestas maecenas pharetra convallis posuere morbi leo urna molestie. Nullam
-                            vehicula ipsum a arcu cursus. Praesent elementum facilisis leo vel.
-                        </Typography>
+                        <Card>
+                            <CardMedia
+                                component={"img"}
+                                image={About1}
+                                title="Contemplative Reptile"
+                                height={400}
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h3" component="h3">
+                                    About Us
+                                </Typography>
+                                <Typography variant="body1" component="p">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua. Urna condimentum mattis pellentesque id
+                                    nibh tortor id aliquet lectus. Egestas maecenas pharetra convallis posuere morbi leo
+                                    urna molestie. Nullam vehicula ipsum a arcu cursus. Praesent elementum facilisis leo
+                                    vel.
+                                    <br/>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua. Urna condimentum mattis pellentesque id
+                                    nibh tortor id aliquet lectus. Egestas maecenas pharetra convallis posuere morbi leo
+                                    urna molestie. Nullam vehicula ipsum a arcu cursus. Praesent elementum facilisis leo
+                                    vel.
+                                    <br/>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua. Urna condimentum mattis pellentesque id
+                                    nibh tortor id aliquet lectus. Egestas maecenas pharetra convallis posuere morbi leo
+                                    urna molestie. Nullam vehicula ipsum a arcu cursus. Praesent elementum facilisis leo
+                                    vel.
+                                </Typography>
+                            </CardContent>
+                        </Card>
                     </Grid>
                 </Grid>
-
-
             </Box>
 
         </Container>
