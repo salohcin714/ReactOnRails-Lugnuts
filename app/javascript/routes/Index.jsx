91c5e4bd2d75e7f9ae0d28ee7036e8611799e7f2
Shannon Menard <menard01@email.franklin.edu>
2020-03-17 18:01:55 -0400

    Added checkout component and pages Added route to check out - must fix

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -3,7 +3,8 @@ import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Home from "../components/Home";
 import About from "../components/About";
 import Shop from "../components/Shop";
-import Contact from "../components/Contact"
+import Contact from "../components/Contact";
+import Checkout from "../components/Checkout";
 
 export default function Routes() {
     return (
@@ -20,6 +21,9 @@ export default function Routes() {
             </Route>
             <Route path="/contact">
                 <Contact/>
+            </Route>
+            <Route path="/checkout">
+                <Checkout />
             </Route>
         </Switch>
     </Router>
