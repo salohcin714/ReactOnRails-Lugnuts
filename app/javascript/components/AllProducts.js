50c552a5075cfe6d3fefbaeb3a10bdac50f310fe
Shannon Menard <menard01@email.franklin.edu>
2020-03-22 22:00:51 -0400

    Finished search filters UI

--- app/javascript/components/AllProducts.js
+++ app/javascript/components/AllProducts.js
@@ -10,6 +10,8 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import {makeStyles} from '@material-ui/core/styles';
 import Slider from '@material-ui/core/Slider';
+import Typography from "@material-ui/core/Typography";
+import ButtonBase from '@material-ui/core/ButtonBase';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -25,6 +27,10 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
+function valueText(value) {
+    return '${value}s';
+}
+
 export default function AllProducts() {
     const classes = useStyles();
 
@@ -32,8 +38,14 @@ export default function AllProducts() {
 
     });
 
+    const [value, setValue] = React.useState([1800, 2000]);
+
     const handleChange = event => {
         setState({ ...state, [event.target.name]: event.target.checked });
+    }
+
+    const handleSlider = (event, newValue) => {
+        setValue(newValue);
     }
 
 
@@ -99,6 +111,35 @@ export default function AllProducts() {
                                 <FormControlLabel
                                     control={<Checkbox onChange={handleChange} />}
                                     label="Vintage Cars"
+                                />
+                            </FormGroup>
+                        </FormControl>
+
+                        <Typography id="range-slider" gutterBottom>
+                            Year
+                        </Typography>
+                            <Slider
+                                value={value}
+                                onChange={handleSlider}
+                                valueLabelDisplay="auto"
+                                aria-labelledby="range-slider"
+                                getAriaValueText={valueText}
+                            />
+
+                        <FormControl component="fieldset" className={classes.formControl}>
+                            <FormLabel component="legend">Buy Price</FormLabel>
+                            <FormGroup>
+                                <FormControlLabel
+                                    control={<Checkbox onChange={handleChange} />}
+                                    label="Under $50"
+                                />
+                                <FormControlLabel
+                                    control={<Checkbox onChange={handleChange} />}
+                                    label="$50 - $100"
+                                />
+                                <FormControlLabel
+                                    control={<Checkbox onChange={handleChange} />}
+                                    label="$100 +"
                                 />
                             </FormGroup>
                         </FormControl>
