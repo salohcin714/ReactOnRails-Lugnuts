75564a22db4dc85b66654a486e4c5293a9b3296e
Nicholas Norris <norris48@email.franklin.edu>
2020-04-12 16:17:16 -0400

    Version 0.0.1.4 Optimized Images

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -1,40 +1,36 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
-// of the page.
-
 import React from 'react';
 import {render} from 'react-dom';
 import PropTypes from 'prop-types';
 import App from '../components/App';
-import {Container} from '@material-ui/core';
+import Container from '@material-ui/core/Container';
 import {createMuiTheme} from '@material-ui/core/styles';
 import {ThemeProvider} from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
-    typography: {
-        fontFamily: [
-            'myriad-pro',
-            'Helvetica Neue',
-            'sans-serif',
-        ],
+  typography: {
+    fontFamily: [
+      'myriad-pro',
+      'Helvetica Neue',
+      'sans-serif',
+    ],
+  },
+  palette: {
+    primary: {
+      main: '#4D5F80',
     },
-    palette: {
-        primary: {
-            main: '#4D5F80',
-        },
-        secondary: {
-            main: '#B7312C'
-        }
-    }
+    secondary: {
+      main: '#B7312C',
+    },
+  },
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-    render(
-        <ThemeProvider theme={theme}>
-            <Container maxWidth={'lg'}>
-                <App/>
-            </Container>
-        </ThemeProvider>,
-        document.body.appendChild(document.createElement("div"))
-    );
+document.addEventListener('DOMContentLoaded', () => {
+  render(
+    <ThemeProvider theme={theme}>
+      <Container maxWidth={'lg'}>
+        <App/>
+      </Container>
+    </ThemeProvider>,
+    document.body.appendChild(document.createElement('div')),
+  );
 });
