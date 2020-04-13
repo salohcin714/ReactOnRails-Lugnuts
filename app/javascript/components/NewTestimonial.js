c46f3d67e349f146ab2a172a5b1a0c1b1d0813e5
Nicholas Norris <norris48@email.franklin.edu>
2020-04-13 15:50:42 -0400

    Version 0.0.1.5 Fixed Heroku API call

--- app/javascript/components/NewTestimonial.js
+++ app/javascript/components/NewTestimonial.js
@@ -26,7 +26,7 @@ export default class NewTestimonial extends Component {
   }
 
   handleSubmit(event) {
-    axios.post('http://localhost:3000/api/v1/testimonials', {
+    axios.post('http://lugnuts-webd495.herokuapp.com/api/v1/testimonials', {
       testimonial: {
         title: this.state.title,
         content: this.state.content,
